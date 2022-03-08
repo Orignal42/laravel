31c696d55ad0b4104254ef557ddf8833cf4ed709
Orignal42 <rbarriquand@gmail.com>
2022-03-08 12:48:49 +0100

    4

--- app/Http/Controllers/AppartementController.php
+++ app/Http/Controllers/AppartementController.php
@@ -1,20 +1,50 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Appartement;
-use Illuminate\Http\Request;
+
 
 class AppartementController extends Controller
 {
-    public function all() {
+    public function all()
+    {
         $properties = response()
             ->json(Appartement::all());
         return $properties;
     }
 
-    public function detail($id) {
+    public function detail($id)
+    {
         $properties =  response()
             ->json(Appartement::find($id));
         return $properties;
+    }
+
+    public function add()
+    {
+        $properties = response()
+        ->json(Appartement::all());
+                return Appartement::create([
+                    'title' => $properties['title'],
+                    'description' => $properties['description'],
+                ]);
+      
+    }
+
+    public function modify($id)
+
+    {
+        $properties =  response()
+        ->json(Appartement::find($id));
+        return $properties;
+    }
+
+    public function delete($id)
+    {
+        $properties =  response()
+        ->json(Appartement::create($id));
+        $properties->delete();
+        return response()->json(null);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/property/all', [AppartementController::class, 'all']);
 Route::get('/property/detail/{id}', [AppartementController::class, 'detail']);
+Route::PUT('/property/add',[AppartementController::class,'add']);
+Route::POST('/property/modify/{id}',[AppartementController::class,'edit']);
+Route::DELETE('/property/delete/{id}', [AppartementController::class,'delete']);
 // http://lara.test/api/property/all
